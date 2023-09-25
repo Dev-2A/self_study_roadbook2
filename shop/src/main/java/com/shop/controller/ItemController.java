78704cd203e9163bdddb0c150272cfb2b7e37863
Dev-2A <yehee826@gmail.com>
2023-09-25 14:36:55 +0900

    self-study

---
+++ shop/src/main/java/com/shop/controller/ItemController.java
@@ -0,0 +1,18 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+import com.shop.dto.ItemFormDto;
+
+@Controller
+public class ItemController {
+
+    @GetMapping(value = "/admin/item/new")
+    public String itemForm(Model model) {
+        model.addAttribute("itemFormDto", new ItemFormDto());
+        return "item/itemForm";
+    }
+
+}
